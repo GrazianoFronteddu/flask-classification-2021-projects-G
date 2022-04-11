0a59f94bf555743005a15db8134ae0a5e50df3d8
GrazianoFronteddu <gfronteddu98@gmail.com>
2022-04-11 15:27:54 +0200

    In this first commit three files are added without implementing the functionalities: -classifications_upload.py -> this creates a new route necessary for the upload image classification; -classification_output_upload.html: this file will provide the informations for the frontend -classification_upload_select:this file will provide informations for select the image in the frontend

---
+++ app/routes/classifications_upload.py
@@ -0,0 +1,26 @@
+import redis
+from flask import render_template
+from rq import Connection, Queue
+from rq.job import Job
+
+from app import app
+from app.forms.classification_form import ClassificationForm
+from ml.classification_utils import classify_image
+from config import Configuration
+
+config = Configuration()
+
+
+@app.route('/classifications_upload', methods=['GET', 'POST'])
+def classifications_image_upload():
+    """API for selecting and uploading an image from the user computer and running a
+    classification job. Returns the output scores from the
+    model."""
+
+    if request.method == 'POST':
+
+        # TO IMPLEMENT
+
+        return render_template("classification_output_upload_queue.html")
+
+    return render_template('classification_upload_select.html')
